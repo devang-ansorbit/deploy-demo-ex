77677aaf5e23f926b3fd7263e0b6e77d2305b43b
devang-ansorbit <devang.patel@ansorbit.com>
2023-05-02 15:54:10 +0530

    code update

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `)
+// })
+const port = process.env.PORT || 8000;
+
+
+app.listen(port, function check(error) {
+  if (error) {
+    console.log('Erro:', error);
+  } else {
+    console.log('Server Started');
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
